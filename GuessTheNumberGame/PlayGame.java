3d584548cc0fa6ea5a5637284060ea7c9039974d
RooGingy <71571395+RooGingy@users.noreply.github.com>
2023-12-08 03:16:54 -0600

    Adds, Fixes and Issues
    Added:
    - Levels: as you get further and further in the game levels get harder and harder.
    - Attempts : as the levels gets harder the amount of attempts increases.
    - Made game at least playable.
    
    Fixed:
    - Ranges: made it to where the range increase by 2 times the amount per level.
    - Made bot feedback more user friendly.
    
    Issues:
    - Levels is skipping number
    - For attempts it includes 0 attempts as a try.

--- GuessTheNumberGame/PlayGame.java
+++ GuessTheNumberGame/PlayGame.java
@@ -12,57 +12,140 @@ import java.lang.*;
 
 public class PlayGame
 {
-    /**
-     * Random Number generator:
-     * <p>
-     * Picks a number between 1 and 100.
-     * 
-     * @return returns a random number between 1 though 10
-     */
-    public static int random(){
-        return (int)(Math.random() * (10 - 1)) + 1;
-    }
+    static private int range = 10;
+    static private int attempts = 0;
+    static private int level = 0;
 
     /**
-     * Score:
+     * Level Up:
      * <p>
-     * Creates a string for to diplay score.
+     * After user guess the correct number each level
      * 
      * @param userWins gets the amount of user wins.
      * @param botWins gets the amount of bot wins.
      * @return diplays the score.
      */
-    public static String attempts(int attempts){
-        return "Attempt: " + attempts;
+    public static void levelUp(){
+        //Increments what level the user is on;
+        level++;
+        // After level 1 the range will increase.
+        if(level > 1){
+            range *= 2;
+        }
+        
+        attempts = tries(range);
+    }
+
+    // bug with the range.
+    public static int tries(int range){
+        int tries = 0;
+        while(range > 1){
+            range /= 2;
+            tries++;
+        }
+        return tries;
+    }
+
+    public static void playGame(){
+        Scanner input = new Scanner(System.in);
+        int userInt = 0 ;
+        int bot = 0;
+
+        do{
+            levelUp();
+            bot = (int)(Math.random() * (range - 1)) + 1;
+            System.out.println("Level: " + level);
+            System.out.println("You have " + attempts + " attempts");
+            System.out.print("Ener a number between 1 through " + range+ ": ");
+            userInt = input.nextInt();
+            System.out.println();
+
+
+            while(userInt != bot && attempts != 0){
+                if(userInt < 1 || userInt > range){
+                    System.out.println("Number outside range!");
+                    System.out.print("Ener a number between 1 through " + range+ ": ");
+                    userInt = input.nextInt();
+                    System.out.println();
+
+                }
+                else if(userInt < bot){
+                    attempts--;
+                    System.out.println("Number is too low!");
+                    System.out.println("You have " + attempts + " attempts");
+                    System.out.print("Try again: ");
+                    userInt = input.nextInt();
+                    System.out.println();
+
+                }
+                else if(userInt > bot){
+                    attempts--;
+                    System.out.println("Number is too high!");
+                    System.out.println("You have " + attempts + " attempts");
+                    System.out.print("Try again: ");
+                    userInt = input.nextInt();
+                    System.out.println();
+
+                }
+            }
+
+            if(userInt == bot){
+                System.out.println("Correct!");
+                System.out.println();
+                System.out.println("You have now advanced to the next level!");
+                levelUp();
+                playGame();
+            }
+            else{
+                System.out.println("Game over!");
+
+            }
+        } while(attempts != 0);
+
     }
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        int user = 0;
-        System.out.println("Guess the correct number between 1 though 100.");
+        String userString = " ";
+        int userInt = 0 ;
+        boolean started = false;
+        boolean stopGame = false;
+        int bot = 0;
         do{
-            int random = random(); // Generate a new random number.
-            int attempts = 1; // reset attempt to 1
-            System.out.println(attempts(attempts));
-            System.out.print("Enter a number: ");
-            user = input.nextInt();
+            if (started == false){
+                // Ask user to enter start.
+                System.out.print("Type START to play game: ");
+                userString = input.next().toUpperCase();
+                System.out.println();
 
-            while(!(user == random || attempts == 10))
-            {
-                attempts++;
-                if(user < random){
-                    System.out.println("Number Is Too Low!");
-                    System.out.println(attempts(attempts));
-                    System.out.print("Try Again: ");
-                    user = input.nextInt();
-                }
-                else if(user > random){
-                    System.out.println("Number Is Too High!");
-                    System.out.println(attempts(attempts));
-                    System.out.print("Try Again: ");
-                    user = input.nextInt();
+                // If it started then game will begin.
+                if(userString.equals("START")){
+                    playGame();
+                    started = true;
                 }
             }
-        } while(user != -1);
+            else{
+                do{
+                    System.out.println("Do you want to play again?");
+                    System.out.println("Enter Y for: Yes");
+                    System.out.println("Enter N for: No ");
+                    userString = input.nextLine();
+                    System.out.println();
+
+                    if(userString.toUpperCase().equals("Y")){
+                        playGame();
+                    }
+                    else{
+                        break;
+                    }
+
+                } while(!(userString.toUpperCase().equals("Y") || userString.toUpperCase().equals("N")));
+                if(userString.equals("N")){
+                    break;
+                }
+            }
+        } while(!(userString.equals("START")));
+
+        System.out.println("Game has stopped running.");
     }
 }
